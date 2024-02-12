42c3e2ee1047d7dc85dace1aee64a778e4e7cb25
Serpa <jhonatanmeireles12@yahoo.com.br>
2024-02-12 19:53:06 -0300

    Incluindo o using que é herdado do IDisposable e incluindo no método escrever o limite em que o arquivo deverá ser lido com o limite de bytes

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -6,25 +6,34 @@ class Program
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
-        
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
 
-        var buffer = new byte[1024];
-        
-        while( numeroDeBytesLidos != 0)
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
+            var numeroDeBytesLidos = -1;
 
-        }
+
+            var buffer = new byte[1024];
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+
+            }
+
+            fluxoDoArquivo.Close();
+            Console.ReadLine();
+
+        };
+
+       
 
     }
 
-    static void EscreverBuffer(byte[] buffer)
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
     {
         var utf8 = new UTF8Encoding();
-        var texto = utf8.GetString(buffer);
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
         Console.Write(texto);
         /*
         foreach(var meuByte in buffer)
