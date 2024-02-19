88c802917c42a4dae60cc4f12c752450b57473b8
Serpa <jhonatanmeireles12@yahoo.com.br>
2024-02-18 23:31:09 -0300

    Mais escrita de arquivo

--- ByteBankIO/3_CriandoArquivo.cs
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -9,12 +9,23 @@ partial class Program
 
         using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
         {
-            var contaComoString = "456,7895, 4785.40,Gustavo Santos";
+            var contaComoString = "456,7895, 4785.40,Gustávo Santos";
             var encoding = Encoding.UTF8;
 
             var bytes = encoding.GetBytes(contaComoString);
             fluxoDeArquivo.Write(bytes, 0, bytes.Length);
         }
+    }
+    static void CriarArquivoComWriter()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using(var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            escritor.Write("456,65465,456.0,Pedro");
+        }
+
+
     }
 
 
